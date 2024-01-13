1ef9c37525a8d80577411416c960e0c778a9f767
Aman Barar <barar.amanjeet.aman@gmail.com>
2024-01-14 02:05:41 +0530

    Connected Mongodb

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 const PORT = 3000;
@@ -7,6 +8,20 @@ app.get("/", (req, res) => {
   res.send("Hello Node API");
 });
 
-app.listen(PORT, () => {
-  console.log(`Node API app is running on ${PORT}`);
+app.get("/blog", (req, res) => {
+  res.send("This is blog section");
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:admin@nodeapi.vndlkhk.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Databases connected: MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Node API app is running on ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
