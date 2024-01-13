ec7e2c3bde1514b37d14c39fa583b439fa84408f
Aman Barar <barar.amanjeet.aman@gmail.com>
2024-01-14 02:37:16 +0530

    made API calls in server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+const Product = require("./models/productModel");
+
+app.use(express.json());
 const PORT = 3000;
 
 app.get("/", (req, res) => {
@@ -10,6 +13,35 @@ app.get("/", (req, res) => {
 
 app.get("/blog", (req, res) => {
   res.send("This is blog section");
+});
+
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/product", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 mongoose
