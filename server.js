617783beb7ef3c1554669ca2b5f4c81dba6e0f60
Aman Barar <barar.amanjeet.aman@gmail.com>
2024-01-16 14:31:11 +0530

    add: productRoute api in server.js

--- server.js
+++ server.js
@@ -1,80 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
 require("dotenv").config();
 
-const Product = require("./models/productModel");
-
+const app = express();
+const productRoute = require("./routes/productRoute");
 app.use(express.json());
-const PORT = process.env.PORT;
-
-app.get("/", (req, res) => {
-  res.send("Hello Node API");
-});
-
-app.get("/blog", (req, res) => {
-  res.send("This is blog section");
-});
-
-app.get("/products", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get("/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.post("/product", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.put("/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body);
-    if (!product) {
-      return res
-        .status(404)
-        .json({ message: `cannot find any product with ID ${id}` });
-    }
-    const updatedProduct = await Product.findById(id);
-    res.status(200).json(updatedProduct);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.delete("/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndDelete(id);
-    if (!product) {
-      return res
-        .status(404)
-        .json({ message: `cannot find any product with ID ${id}` });
-    }
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+const PORT = process.env.PORT || 3000;
 
 mongoose
   .connect(process.env.MONGODB_URI, {
@@ -90,3 +21,13 @@ mongoose
   .catch((error) => {
     console.error(error);
   });
+
+app.use("/api", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello Node API");
+});
+
+app.get("/blog", (req, res) => {
+  res.send("This is blog section");
+});
